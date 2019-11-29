8d9bbdeec04a06219915907b329b923dbd2aa4ae
zafaraabid <zafaraabid3295@gmail.com>
2019-11-29 19:12:38 +0530

    refactored code:written 1 more test to throw Exception if fileName is wrong

--- src/test/java/com/statecensus/StateCensusAnalyserTests.java
+++ src/test/java/com/statecensus/StateCensusAnalyserTests.java
@@ -7,14 +7,20 @@ import java.io.IOException;
 
 public class StateCensusAnalyserTests {
     @Test
-    public void WhenReadFile_CountRecords_ifCorrect_ShouldReturnTrue() throws IOException {
+    public void WhenReadFile_CountRecords_ifCorrect_ShouldReturnTrue() throws IOException, CustomException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
-    try {
-        Assert.assertEquals(37,stateCensusAnalyser.getRecordCount());
+        Assert.assertEquals(37, stateCensusAnalyser.getRecordCount());
     }
-    catch (CustomException e)
-    {
-        Assert.assertEquals(CustomException.ExceptionType.NO_SUCH_FILE,e.type);
-    }
+
+    @Test
+    public void WhenReadFile_ifFileNameIsCorrect_ShouldReturnTrue() throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        try {
+            Assert.assertEquals(37,stateCensusAnalyser.getRecordCount());
+        }
+        catch (CustomException e)
+        {
+            Assert.assertEquals(CustomException.ExceptionType.NO_SUCH_FILE,e.type);
+        }
     }
 }
